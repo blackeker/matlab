1cc7aeb75b8d91e0842c6f48d0570a58c6305ffd
blackeker <blackeker@gmail.com>
2026-05-28 21:53:05 +0300

    Otomatik güncelleme yapıldı

---
+++ adim3.m
@@ -0,0 +1,9 @@
+% CÝDDEN YÜZDE ÝÞARETÝMÝi
+% Neyse iþimize bakalým 
+A=3;
+% c ÝLE ayni neredeysede bazi yerleri farkli tek bunu anlasam yeter DÝYE
+% DUSUNUYORUM
+% printf('%d', A);
+% Anani print yokmus
+fprintf('A: %d\n', A);
+% CÝDDEN BASINA F MÝ KOYMAM LAZIMDI
