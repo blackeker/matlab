a423343b2dc172e188ef3f69a8070e4b03eebd01
blackeker <blackeker@gmail.com>
2026-05-28 21:55:41 +0300

    Otomatik güncelleme yapıldı

--- adim3.m
+++ adim3.m
@@ -7,3 +7,11 @@ A=3;
 % Anani print yokmus
 fprintf('A: %d\n', A);
 % CÝDDEN BASINA F MÝ KOYMAM LAZIMDI
+% tamam diger islemlere gecebiliriz diye dusunuyorum dusunmekteyim 
+B = 9;
+C = A+B;
+D = A*B;
+E = A / B;
+fprintf('C: %d\n', C);
+fprintf('D: %d\n', D);
+fprintf('E: %d\n', E);
