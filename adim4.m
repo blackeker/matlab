5c18cac1ac5c5cd5e8b799b32e3f7b24c5dd7520
blackeker <blackeker@gmail.com>
2026-05-28 22:04:55 +0300

    Otomatik güncelleme yapıldı

--- adim4.m
+++ adim4.m
@@ -2,3 +2,7 @@
 A = [ 1 2 3 ; -2 1 6 ]
 % Bisi kesfettim sonuna ; koymazsam ekrana printliyormus 
 %EVET BUNU YENÝ FARKETTÝM
+% dizilerin carpimlari biraz garip birkac farkli carpim turu var cunku 
+B = [ 4 5 6 ; 1 2 2 ]
+C = A*B
+D = A.*B
