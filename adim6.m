d003cda334c693c4a8f3b52ec3544ef0a73783e5
blackeker <blackeker@gmail.com>
2026-05-29 15:05:11 +0300

    Otomatik güncelleme yapıldı

---
+++ adim6.m
@@ -0,0 +1,20 @@
+% Tek tek yazmaya usendimde bunlari heheeh
+
+% Trigonometrik Fonksiyonlar (Radyan cinsinden çalışır)
+sin(pi)     % Sinüs (Sonuç 0'a çok yakın) -> Örnek: sin(pi/2) ekrana 1 basar
+cos(pi)     % Kosinüs (Sonuç: -1) -> Örnek: cos(pi) ekrana -1 basar
+tan(pi)     % Tanjant (Sonuç: 0'a çok yakın) -> Örnek: tan(pi/4) ekrana 1 basar
+
+% Ters Trigonometrik Fonksiyonlar (Sonucu radyan verir)
+asin(0)     % Arksinüs (arcsin) -> Örnek: asin(1) ekrana 1.5708 (pi/2) basar
+acos(0)     % Arkkosinüs (arccos) -> Örnek: acos(1) ekrana 0 basar
+atan(0)     % Arktanjant (arctan) -> Örnek: atan(1) ekrana 0.7854 (pi/4) basar
+
+% Logaritma ve Üstel Fonksiyonlar
+exp(2)      % Eksponensiyel (e^2) -> Örnek: exp(1) ekrana 2.7183 (e sayısı) basar
+log(10)     % Doğal logaritma (ln) -> Örnek: log(exp(1)) ekrana 1 basar
+log10(10)   % 10 tabanlı logaritma -> Örnek: log10(100) ekrana 2 basar
+
+% Diğer Temel Fonksiyonlar
+sqrt(25)    % Karekök (Square Root) -> Örnek: sqrt(16) ekrana 4 basar
+abs(3)      % Mutlak değer (Absolute Value) -> Örnek: abs(-5) ekrana 5 basar
