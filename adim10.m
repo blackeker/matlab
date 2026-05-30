13d3b9fc4bd8d83943e64141bfb1e54efa10057e
blackeker <blackeker@gmail.com>
2026-05-30 19:25:39 +0300

    Otomatik güncelleme yapıldı

--- adim10.m
+++ adim10.m
@@ -2,9 +2,12 @@ t = 0:0.01:2*pi;
 x = sin(t);
 y = cos(t);
 
-plot(t, x, '-', t, y, '-');
-grid ___;
-xlabel('___');
-ylabel('___');
-title('-');
-legend('___', '___');
+% x grafiði mavi sürekli ('b-'), y grafiði kýrmýzý kesikli ('r--') çizgi
+plot(t, x, 'b-', t, y, 'r--');
+
+% Grafik biçimlendirmeleri
+grid on;                      % Grid açýk
+xlabel('t');                  % X ekseni etiketi
+ylabel('x and y');            % Y ekseni etiketi
+title('sine and cosine curves'); % Grafik baþlýðý
+legend('sine', 'cosine');     % Sýrasýyla legend isimleri
