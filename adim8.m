00540204abd5a19fc4396f3febaff75663f10482
blackeker <blackeker@gmail.com>
2026-05-30 19:16:04 +0300

    Otomatik güncelleme yapıldı

--- adim8.m
+++ adim8.m
@@ -1,4 +1,7 @@
+% İlk quizin yanıtları bunlar determinant için det(degisken) komudunu matrisin tersi icin inv(degisken) kullanıyoz 
 A = [ 1 3 5 ; 7 8 11 ; 100 1 4]
+det(A)
 B = [10 5 -5 ; 70 8 7 ; 10 1 3]
 B = inv(A)
 A * B
+A(3,:) = [5 6 7]
