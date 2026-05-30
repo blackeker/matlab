7b09c6af9f0bd98f412ae3271d6b4b7b5adf0997
blackeker <blackeker@gmail.com>
2026-05-30 18:13:31 +0300

    Otomatik güncelleme yapıldı

--- adim7.m
+++ adim7.m
@@ -2,3 +2,9 @@
 A = 99+1*18+3
 B = (99+1)*18+3
 % Yani boyle baska bisi yok 
+%  Bisi kesfettim ctrl + r yapýnca yorum satýrýna aliyormus aw
+% Neyse 
+test = input('test:')
+%  Boylede input atabiliyormusuz VAY ANASÝNÝ AFKLNSGFKB
+%  cye fazla benzemiyor ama neyseeee
+%  Hocanin sordugu quizlere gecebiliriz 
